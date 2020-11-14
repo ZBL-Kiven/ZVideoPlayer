0cd49d76cc4735407fb76a69b2a2d897841cc0fa
Junjie.Zhao <570738660@qq.com>
2020-11-14 23:56:56 +0800

    supported full screen configs

--- player/src/main/java/com/zj/player/full/Transaction.kt
+++ player/src/main/java/com/zj/player/full/Transaction.kt
@@ -1,7 +1,7 @@
 package com.zj.player.full
 
 @Suppress("unused")
-class Transaction(internal val formUser: Boolean, fullScreenTransactionTime: Int, startOnly: Boolean, val payloads: Map<String, Any?>? = null) {
+class Transaction(val formUser: Boolean, fullScreenTransactionTime: Int, startOnly: Boolean, val payloads: Map<String, Any?>? = null) {
     var transactionTime: Int = fullScreenTransactionTime
     var isStartOnly: Boolean = startOnly
 }
