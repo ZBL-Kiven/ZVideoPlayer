d0a97051923b5227de0eabd147bb0892c06da61c
Junjie.Zhao <570738660@qq.com>
2021-04-09 22:00:51 +0800

    fix reload if list used

--- player/src/main/java/com/zj/player/controller/BaseListVideoController.kt
+++ player/src/main/java/com/zj/player/controller/BaseListVideoController.kt
@@ -55,7 +55,7 @@ abstract class BaseListVideoController @JvmOverloads constructor(c: Context, att
     }
 
     private fun load(v: View, reload: Boolean, fromUser: Boolean) {
-        if (controller?.isPlaying() == true) {
+        if (controller?.isLoadData() == true) {
             if (reload) super.reload(v) else super.onPlayClick(v, false)
         } else {
             videoControllerIn?.waitingForPlay(curPlayingIndex, 20L, fromUser) ?: controller?.let {
